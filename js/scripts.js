1a98deeb837ae59e1568cb06b905f07d58511fb8
Omri93h <Omri93h@gmail.com>
2020-09-21 22:09:07 +0300

    Improving responsiveness + live calculating

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
-// $(document).ready(function() {
-// console.log(6)
+var btc_price, api_data;
 
-// });
-
-var settings = {
+var crypto_settings = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://coinpaprika1.p.rapidapi.com/tickers",
@@ -14,7 +11,77 @@ var settings = {
 	}
 }
 
-$.ajax(settings).done(function (response) {
-    $("#btc-price").html(response[0]["quotes"]["USD"]["price"]);
-    $("#converted").html(response[0]["quotes"]["USD"]["price"]);
+
+function Calculate() {
+	var base_coin = $('#crypto-currencies option:selected').val();
+	var convert_coin = $('#fiat-currencies option:selected').val();
+	var value_to_convert = $("#to-convert").val();
+	if(converted_coin == "EUR"){
+
+	}
+	var final_val = to_convert * value
+	$("#converted").html(makeReadableFloat(final_val));
+}
+
+$.ajax(crypto_settings).done(function (response) {
+	$("#converted").html(makeReadableFloat(response[0]["quotes"]["USD"]["price"]));
 });
+
+
+function makeReadableFloat(val) {
+	return (Number(parseFloat(val).toFixed(3)).toLocaleString());
+}
+
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+async function updateValues() {
+	while (true) {
+		var i = 0;
+		$.ajax(crypto_settings).done(function (response) {
+			function getBTC() {
+				btc_price = response[0]["quotes"]["USD"]["price"];
+				return btc_price;
+			};
+			btc_price = getBTC();
+			$("#btc-price").css("display", "none");
+			$("#btc-price").html("BTC Value: " + makeReadableFloat(btc_price) + "$").fadeIn(200);
+		});
+
+		while (i < 10) {
+			await sleep(1 * 1000);
+			$("#updated").html("Auto Update In " + (10-i));
+			i++;
+		}
+	}
+}
+updateValues();
+
+
+
+
+var fiat_settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://currency-exchange.p.rapidapi.com/exchange?from=USD&to=EUR",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+		"x-rapidapi-key": "289ecf230bmshe9159cc82bc2183p1cee5ejsndcbb688e0224"
+	}
+}
+
+$.ajax(fiat_settings).done(function (response) {
+	console.log(response);
+	console.log("CHECKKKKKKKKKKKKKKKKKKKK");
+});
+
+
+
+
+
+
